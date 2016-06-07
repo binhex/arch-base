c3b1f2a0884f68105e0c082b810374949729aa6a
binhex <megalith01@gmail.com>
2016-06-07 16:40:28 +0100

    add in fallback arch archive to mirrorlist

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -8,6 +8,7 @@ yesterdays_date=$(date -d "yesterday" +%Y/%m/%d)
 
 # now set pacman to use snapshot for packages for yesterdays date
 echo 'Server = https://archive.archlinux.org/repos/'"${yesterdays_date}"'/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = https://ala.seblu.net/repos/'"${yesterdays_date}"'/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 
 echo "[info] content of arch mirrorlist file"
 cat /etc/pacman.d/mirrorlist

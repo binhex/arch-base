c3cedd246191bf3d18fe022b67a3f76d210bb7d3
binhex <megalith01@gmail.com>
2017-01-01 23:12:31 +0000

    move refresh of keys to top

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -13,6 +13,18 @@ echo 'Server = https://ala.seblu.net/repos/'"${yesterdays_date}"'/$repo/os/$arch
 echo "[info] content of arch mirrorlist file"
 cat /etc/pacman.d/mirrorlist
 
+# upgrade pacman db
+pacman-db-upgrade
+
+# delete any local keys
+rm -rf /root/.gnupg
+
+# force re-creation of /root/.gnupg and start dirmgr
+dirmngr </dev/null
+
+# refresh keys for pacman
+pacman-key --refresh-keys
+
 # update packages ignoring filesystem (docker limitation)
 pacman -Syu --ignore filesystem --noconfirm
 
@@ -35,18 +47,6 @@ chmod -R 775 /home/nobody
 # set shell for user nobody
 chsh -s /bin/bash nobody
  
-# upgrade pacman db
-pacman-db-upgrade
-
-# delete any local keys
-rm -rf /root/.gnupg
-
-# force re-creation of /root/.gnupg and start dirmgr
-dirmngr </dev/null
-
-# refresh keys for pacman
-pacman-key --refresh-keys
-
 # force re-install of ncurses 6.x with 5.x backwards compatibility (can be removed onced all apps have switched over to ncurses 6.x)
 curl -o /tmp/ncurses5-compat-libs-x86_64.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/ncurses5-compat-libs-x86_64.pkg.tar.xz
 pacman -U /tmp/ncurses5-compat-libs-x86_64.pkg.tar.xz --noconfirm

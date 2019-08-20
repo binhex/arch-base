e8458e9d290d01d61c7d9bc5b9ff2bf98d600c8b
binhex <megalith01@gmail.com>
2019-08-20 11:35:50 +0100

    add in cleanup for base-devel

--- build/root/install.sh
+++ build/root/install.sh
@@ -154,14 +154,23 @@ tini_release_tag=$(cat /tmp/tini_release_tag | grep -P -o -m 1 '(?<=/krallin/tin
 # download tini, used to do graceful exit when docker stop issued and correct reaping of zombie processes.
 curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /usr/bin/tini -L "https://github.com/krallin/tini/releases/download/${tini_release_tag}/tini-amd64" && chmod +x /usr/bin/tini
 
-# cleanup
+# identify if base-devel package installed
+if pacman -Qg "base-devel" > /dev/null ; then
+
+	# remove base devel excluding useful core packages
+	pacman -Ru $(pacman -Qgq base-devel | grep -v awk | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip | grep -v which) --noconfirm
+
+fi
+
+# general cleanup
 yes|pacman -Scc
 pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
-rm -rf \
-/usr/share/locale/* \
-/usr/share/man/* \
-/usr/share/gtk-doc/* \
-/root/* \
-/tmp/* \
-/var/cache/pacman/pkg/* \
-/README
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /usr/share/gtk-doc/*
+rm -rf /tmp/*
+
+# additional cleanup for base only
+rm -rf /root/*
+rm -rf /var/cache/pacman/pkg/*
+rm -rf /README

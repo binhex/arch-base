fccf4180e84c1aba892bad289e0c459116bbf485
binhex <megalith01@gmail.com>
2020-04-30 15:11:54 +0100

    additional cleanup

--- build/root/install.sh
+++ build/root/install.sh
@@ -149,13 +149,20 @@ pacman -Ru dotnet-sdk yarn git yay-bin reflector gcc binutils --noconfirm 2> /de
 # general cleanup
 yes|pacman -Scc
 pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
-rm -rf /var/cache/*
-rm -rf /var/empty/.cache/*
-rm -rf /usr/share/locale/*
-rm -rf /usr/share/man/*
-rm -rf /usr/share/gtk-doc/*
-rm -rf /tmp/*
+rm -rf /var/cache/* \
+/var/empty/.cache/* \
+/usr/share/locale/* \
+/usr/share/man/* \
+/usr/share/gtk-doc/* \
+/tmp/*
 
 # additional cleanup for base only
-rm -rf /root/*
-rm -rf /README
+rm -rf /root/* \
+/var/cache/pacman/pkg/* \
+/usr/lib/firmware \
+/usr/lib/modules \
+/bootstrap \
+/.dockerenv \
+/.dockerinit \
+/usr/share/info/* \
+/README

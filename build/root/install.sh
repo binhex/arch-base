fe20f7190d9ff47e1e2ed766030b928628b97117
binhex <megalith01@gmail.com>
2021-09-06 12:08:34 +0100

    comment change

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,8 +3,8 @@
 # exit script if return code != 0
 set -e
 
-# construct snapshot date (cannot use todays as archive wont exist) and set url for archive
-# note for arch linux arm archive repo that the snapshot date has to be at least 2 days
+# construct snapshot date (cannot use todays as archive wont exist) and set url for archive.
+# note: for arch linux arm archive repo that the snapshot date has to be at least 2 days
 # previous as the mirror from live to the archive for arm packages is slow
 snapshot_date=$(date -d "2 days ago" +%Y/%m/%d)
 
